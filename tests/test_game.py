ab4d42720e3135536760854db2e91121f8955b57
stephane <contact@ze-ogier.com>
2024-02-20 18:59:58 +0000

    Game development with TDD

---
+++ tests/test_game.py
@@ -0,0 +1,47 @@
+from longest_word.game import Game
+import string
+
+class TestGame:
+    def test_game_initialization(self):
+            # setup
+            new_game = Game()
+
+            # exercise
+            grid = new_game.grid
+
+            # verify
+            assert type(grid) == list
+            assert len(grid) == 9
+            for letter in grid:
+                assert letter in string.ascii_uppercase
+
+    def test_empty_word_is_invalid(self):
+        # setup
+        new_game = Game()
+        # verify
+        assert new_game.is_valid('') is False
+
+
+    def test_is_valid(self):
+        # setup
+        new_game = Game()
+        test_grid = 'KWEUEAKRZ'
+        test_word = 'EUREKA'
+        # exercice
+        new_game.grid = list(test_grid) # Force the grid to a test case
+        # verify
+        assert new_game.is_valid(test_word) is True
+        # teardown
+        assert new_game.grid == list(test_grid) # Make sure the grid remained untouched
+
+    def test_is_invalid(self):
+        # setup
+        new_game = Game()
+        test_grid = 'KWEUEAKRZ'
+        test_word = 'SANDWICH'
+        # exerice
+        new_game.grid = list(test_grid) # Force the grid to a test case
+        # verify
+        assert new_game.is_valid(test_word) is False
+        # teardown
+        assert new_game.grid == list(test_grid) # Make sure the grid remained untouched
