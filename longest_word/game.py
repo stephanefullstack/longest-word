abb83538be40c5880e0aae357175c6fc7ca9ca8a
stephane <contact@ze-ogier.com>
2024-02-20 19:47:07 +0000

    Feature complete: Dictionary check of attempt

--- longest_word/game.py
+++ longest_word/game.py
@@ -20,3 +20,10 @@ class Game:
             else:
                 return False
         return True
+        return self.__check_dictionary(word)
+
+    @staticmethod
+    def __check_dictionary(word):
+        response = requests.get(f"https://wagon-dictionary.herokuapp.com/{word}")
+        json_response = response.json()
+        return json_response['found']
